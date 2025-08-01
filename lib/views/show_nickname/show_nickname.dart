1d23dd24ca4c58e57857f19e8347a6c8f4caa182
JEEL <jeelhemangi2000@gmail.com>
2025-08-01 21:28:43 +0530

    change in shownickname

--- lib/views/show_nickname/show_nickname.dart
+++ lib/views/show_nickname/show_nickname.dart
@@ -5,15 +5,12 @@ import 'package:kahoot_app/constants/app_colors.dart';
 import 'package:kahoot_app/constants/app_images.dart';
 import 'package:kahoot_app/views/show_nickname/show_nickname_controller.dart';
 
-
 class ShowNicknameScreen extends GetView<ShowNickNameController> {
   const ShowNicknameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildBody(controller.initial, controller.fullName),
-    );
+    return Scaffold(body: _buildBody(controller.initial, controller.fullName));
   }
 
   Widget _buildBody(String initial, String fullName) {
@@ -22,37 +19,46 @@ class ShowNicknameScreen extends GetView<ShowNickNameController> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircleAvatar(
-            radius: 40,
-            backgroundColor: AppColors().purple700,
-            child: Text(
-              initial,
-              style: const TextStyle(
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          Text(
-            fullName,
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: AppColors().white,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "You're in! See your nickname on screen?",
-            style: TextStyle(
-              fontSize: 16,
-              color: AppColors().white.withOpacity(0.9),
-            ),
-          ),
+          _letterInitial(initial),
+          SizedBox(height: 16),
+          _fullNameText(fullName),
+          SizedBox(height: 10),
+          _messageText(),
         ],
       ),
+    );
+  }
+
+  Widget _letterInitial(String initial) {
+    return CircleAvatar(
+      radius: 40,
+      backgroundColor: AppColors().purple700,
+      child: Text(
+        initial,
+        style: TextStyle(
+          fontSize: 40,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _fullNameText(String fullName) {
+    return Text(
+      fullName,
+      style: TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+        color: AppColors().white,
+      ),
+    );
+  }
+
+  Widget _messageText() {
+    return Text(
+      "You're in! See your nickname on screen?",
+      style: TextStyle(fontSize: 16, color: AppColors().white.withOpacity(0.9)),
     );
   }
 }
