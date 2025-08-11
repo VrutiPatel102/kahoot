8ca0e50e4e0745b5188259b122ad035d562661b3
JEEL <jeelhemangi2000@gmail.com>
2025-08-11 16:02:19 +0530

    change in score status screen

--- lib/views/score_status/score_status_controller.dart
+++ lib/views/score_status/score_status_controller.dart
@@ -20,14 +20,11 @@ class ScoreStatusController extends GetxController {
   void simulateLoading() async {
     await Future.delayed(const Duration(seconds: 3));
 
-    // Increase loop count
     QueLoadingController.loopCount++;
 
     if (QueLoadingController.loopCount < 3) {
-      // Go to QueLoading again
       Get.offNamed(AppRoute.queLoading, preventDuplicates: false);
     } else {
-      // After 3 cycles → User Rank screen
       Get.offNamed(AppRoute.userRank);
     }
   }
